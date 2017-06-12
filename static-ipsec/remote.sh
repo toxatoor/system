81017e19d482a3de522c50ca2376a98e8e23f47d
Anton Noginov <anton.noginov@gmail.com>
2017-06-12 14:49:37 +0200

    Removed ip assignment

--- static-ipsec/remote.sh
+++ static-ipsec/remote.sh
@@ -14,4 +14,3 @@
 /sbin/ip l2tp add tunnel local $REMOTE remote $LOCAL tunnel_id 2 peer_tunnel_id 1 encap udp udp_sport $RPORT udp_dport $LPORT
 /sbin/ip l2tp add session name l2tp0 session_id 2 peer_session_id 1 tunnel_id 2
 /sbin/ip link set dev l2tp0 up
-/sbin/ip addr add 10.128.128.2/29 dev l2tp0
