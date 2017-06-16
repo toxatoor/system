67d25beff0e1cf642eb645619bf70eb92dfabdab
Anton Noginov <anton.noginov@gmail.com>
2017-06-16 17:12:26 +0200

    Fix to make mac-address generation working in virtualized environment

--- lxc-dev-cloud/bin/create.sh
+++ lxc-dev-cloud/bin/create.sh
@@ -3,7 +3,8 @@
 name=$1
 
 lxc-create -n $name -f config -t download -- -d centos -r 7 -a amd64
-mac=`uuid -d $(uuid) | grep 'node:'   | awk '{print $2}' | awk -F':' '{print "aa:bb:"$4":"$1":"$2":"$3}'`
+# mac=`uuid -d $(uuid) | grep 'node:'   | awk '{print $2}' | awk -F':' '{print "aa:bb:"$4":"$1":"$2":"$3}'`
+mac=$(dd if=/dev/random bs=4 count=1 2>/dev/null | xxd -p | sed 's/^\(..\)\(..\)\(..\)\(..\)/aa:bb:\1:\2:\3\:\4/')
 echo -ne "lxc.network.hwaddr = $mac\n"  >> /var/lib/lxc/$name/config 
 lxc-start -d -n $name 
 lxc-attach -n $name -- /bin/ln -s /usr/lib/systemd/system/halt.target /etc/systemd/system/sigpwr.target && systemctl daemon-reload
